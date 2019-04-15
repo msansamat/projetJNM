0cb6b1df22eff557bc3d788a6e24484ce6ecd731
msansamat <m.sansamat@gmail.com>
2019-04-15 14:57:00 +0200

    Push JS

---
+++ js/js/main.js
@@ -0,0 +1,420 @@
+jQuery(function($) {
+	"use strict";
+
+	var owlPricing;
+	var ratio = 2;
+
+	// Window Load
+	$(window).load(function() {
+		// Preloader
+		$('.intro-tables, .parallax, header').css('opacity', '0');
+		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			$('.preloader').hide();
+			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+			});
+		});
+
+		// Header Init
+		if ($(window).height() > $(window).width()) {
+			var ratio = $('.parallax').width() / $('.parallax').height();
+			$('.parallax img').css('height', ($(window).height()) + 'px');
+			$('.parallax img').css('width', $('.parallax').height() * ratio + 'px');
+		}
+
+		$('header').height($(window).height() + 80);
+		$('section .cut').each(function() {
+			if ($(this).hasClass('cut-top'))
+				$(this).css('border-right-width', $(this).parent().width() + "px");
+			else if ($(this).hasClass('cut-bottom'))
+				$(this).css('border-left-width', $(this).parent().width() + "px");
+		});
+
+
+		// Navbar Init
+		$('nav').addClass('original').clone().insertAfter('nav').addClass('navbar-fixed-top').css('position', 'fixed').css('top', '0').css('margin-top', '0').removeClass('original');
+		$('.mobile-nav ul').html($('nav .navbar-nav').html());
+		$('nav.navbar-fixed-top .navbar-brand img').attr('src', $('nav.navbar-fixed-top .navbar-brand img').data("active-url"));
+
+		// TypeWriter réglages initiaux 
+		$(".typed").typewriter({
+			speed: 60
+		});
+
+		// Réglage vitesse du scroll
+		$('.navbar.navbar-fixed-top .navbar-nav').onePageNav({
+			currentClass: 'active',
+			changeHash: false,
+			scrollSpeed: 400,
+			filter: ':not(.btn)'
+		});
+	});
+	// Changement couleur navbar apres scroll
+	function onScroll() {
+		if ($(window).scrollTop() > 50) {
+			$('nav.original').css('opacity', '0');
+			$('nav.navbar-fixed-top').css('opacity', '1');
+		} else {
+			$('nav.original').css('opacity', '1');
+			$('nav.navbar-fixed-top').css('opacity', '0');
+		}
+	}
+
+	window.addEventListener('scroll', onScroll, false, {passive: true});
+
+	// Adaptation taille header
+	$(window).resize(function() {
+		$('header').height($(window).height());
+	});
+
+
+	// Navigation mobile
+	$('body').on('click', 'nav .navbar-toggle', function() {
+		event.stopPropagation();
+		$('.mobile-nav').addClass('active');
+	});
+
+	$('body').on('click', '.mobile-nav a', function(event) {
+		$('.mobile-nav').removeClass('active');
+		if(!this.hash) return;
+		event.preventDefault();
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			event.stopPropagation();
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+	$('body').on('click', '.mobile-nav a.close-link', function(event) {
+		$('.mobile-nav').removeClass('active');
+		event.preventDefault();
+	});
+
+	$('body').on('click', 'nav.original .navbar-nav a:not([data-toggle])', function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			event.stopPropagation();
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+
+});
+
+
+//ParticlesJSParametres
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#888"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#777",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
+
+
+//Typewriter
+
+(function($, w, d, undefined) {
+
+  function typewriter() {
+
+    // Globals 
+    var self = this, speed;
+
+    function init(element, options) {
+            // Set Globals
+      var str;
+      var indice = 0;
+
+      self.options = $.extend( {}, $.fn.typewriter.options, options );
+      $currentElement = $(element);
+      elementStr = $currentElement.text().replace(/\s+/g, ' ');
+      dataSpeed  = $currentElement.data("speed") || self.options.speed;
+      $currentElement.empty();
+      var showText = setInterval(
+				function(){
+					if (indice++ < elementStr.length) {
+			      $currentElement.append(elementStr[indice - 1]);
+			    }else{
+			    	clearInterval(showText);
+			    }
+				}, dataSpeed);
+      // self.animation = setInterval(function(){animate_calification()}, 20);
+    }
+
+    
+    
+    // Metodos publicos
+    return {
+      init: init
+    }
+  }
+
+  // Plugin jQuery
+  $.fn.typewriter = function(options) {
+    return this.each(function () {
+    	var writer =  new typewriter();
+      writer.init(this, options);
+      $.data( this, 'typewriter', writer);
+    });
+  };
+
+  $.fn.typewriter.options = {
+    'speed' : 300
+  };
+
+})(jQuery, window, document);
+
+
+
+
+$(document).ready(function() {
+
+  /* Carouser section déroulement
+  -----------------------------------------------*/
+  $(document).ready(function() {
+      $("#team-carousel").owlCarousel({
+          items : 3,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3],
+          slideSpeed: 300,
+          itemsDesktop : [1199,2],
+          itemsTablet: [768,1],
+          itemsTabletSmall: [985,2],
+          itemsMobile : [479,1],
+      });
+    });
+    
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
+
+/* ----------------------------------------------------------- */
+  /*  7. COUNTER
+  /* ----------------------------------------------------------- */
+
+(function( $ ){
+  "use strict";
+
+  $.fn.counterUp = function( options ) {
+
+    // Defaults
+    var settings = $.extend({
+        'time': 400,
+        'delay': 10
+    }, options);
+
+    return this.each(function(){
+
+        // Store the object
+        var $this = $(this);
+        var $settings = settings;
+
+        var counterUpper = function() {
+            var nums = [];
+            var divisions = $settings.time / $settings.delay;
+            var num = $this.text();
+            var isComma = /[0-9]+,[0-9]+/.test(num);
+            num = num.replace(/,/g, '');
+            var isInt = /^[0-9]+$/.test(num);
+            var isFloat = /^[0-9]+\.[0-9]+$/.test(num);
+            var decimalPlaces = isFloat ? (num.split('.')[1] || []).length : 0;
+
+            // Generate list of incremental numbers to display
+            for (var i = divisions; i >= 1; i--) {
+
+                // Preserve as int if input was int
+                var newNum = parseInt(num / divisions * i);
+
+                // Preserve float if input was float
+                if (isFloat) {
+                    newNum = parseFloat(num / divisions * i).toFixed(decimalPlaces);
+                }
+
+                // Preserve commas if input had commas
+                if (isComma) {
+                    while (/(\d+)(\d{3})/.test(newNum.toString())) {
+                        newNum = newNum.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+                    }
+                }
+
+                nums.unshift(newNum);
+            }
+
+            $this.data('counterup-nums', nums);
+            $this.text('0');
+
+            // Updates the number until we're done
+            var f = function() {
+                $this.text($this.data('counterup-nums').shift());
+                if ($this.data('counterup-nums').length) {
+                    setTimeout($this.data('counterup-func'), $settings.delay);
+                } else {
+                    delete $this.data('counterup-nums');
+                    $this.data('counterup-nums', null);
+                    $this.data('counterup-func', null);
+                }
+            };
+            $this.data('counterup-func', f);
+
+            // Start the count up
+            setTimeout($this.data('counterup-func'), $settings.delay);
+        };
+
+        // Perform counts when the element gets into view
+        $this.waypoint(counterUpper, { offset: '30%', triggerOnce: true });
+    });
+
+  };
+
+})( jQuery );
+
+
+
+jQuery(function($){
+      jQuery('.counter').counterUp({
+        delay: 3,
+        time: 1000
+    });
+});
+
+
+
+
+
+
+
+
+
